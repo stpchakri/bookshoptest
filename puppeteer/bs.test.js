bb33297f3d83f211e5ee964d5d08672c68be2d86
chakri <chakravarthy.sreemath.tirumala.pallerlamudi@sap.com>
2020-06-17 15:33:22 +0530

    init

---
+++ puppeteer/bs.test.js
@@ -0,0 +1,63 @@
+const puppeteer = require("puppeteer");
+const screenshotPath = "puppeteer/screenshots/";
+
+let browser;
+let page;
+let i = 1;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch({
+        headless: false,
+        devtools: true
+    });
+    page = await browser.newPage();
+    await page.goto(
+        URL, { waitUntil: "domcontentloaded" }
+    );
+    await page.screenshot({ path: 'Bookshop.png' });
+
+
+});
+
+afterEach(async () => {
+    await page.screenshot({ path: screenshotPath + (++i) + 'Bookshop.png' });
+});
+
+describe("Bookshop Tests", () => {
+    it('Check if Title is Capire Books', async () => {
+        const sTitle = await page.$eval('#app >h1', el => el.innerText);
+        expect(sTitle).toBe('Capire Books');
+    });
+
+    it('Should return correct results on Filter', async () => {
+        const sSearchString = "Bront";
+        await page.type(`input[type="text"]`, sSearchString);
+        const result = await page.$('#app #books tbody tr');
+        const aData = await page.$$eval('#app #books tbody tr', trs => trs.map((tr) => {
+            return tr.innerText;
+        }));
+        console.log(aData);
+        const bCorrectResults =  aData.every((val) => val.includes(sSearchString));
+        debugger;
+        expect(bCorrectResults).toBe(true);
+     });
+
+     it('Should show details on Selection of table row', async () => {
+        const result = await page.$('#app #books tbody tr');
+        const aData = await page.$$eval('#app #books tbody tr', trs => trs.map((tr) => {
+            return tr.innerText;
+        }));
+        console.log(aData);
+        const bCorrectResults =  aData.every((val) => val.includes(sSearchString));
+        debugger;
+        expect(bCorrectResults).toBe(true);
+     });
+
+})
+
+
+afterAll(async () => {
+    browser.close()
+});
+
+
